aa0d02ffe968d00be77e87b18e4fdc0e05c61c38
weloxux <weloxux@users.noreply.github.com>
2016-02-09 10:01:01 +0100

    add enabled variable and give explanations on config items

--- config.php
+++ config.php
@@ -1,8 +1,10 @@
 <?php
-// Site config
-$name = 'LitenWiki';
-$mainpage = 'MainPage';
-$captcha_enabled = false;
-$captcha_text = '7 - 3 =';
-$captcha_answer = '4';
+$name = 'LitenWiki';        // How your wiki will refer to itself
+$mainpage = 'MainPage';     // Landing page when no specific page is requested
+
+$captcha_enabled = false;   // Whether or not captcha is enabled
+$captcha_text = '7 - 3 =';  // Captcha question that needs to be answered if captcha is enabled
+$captcha_answer = '4';      // Answer to the captcha question
+
+$enabled = false;           // Whether or not the wiki is accessible
 ?>
